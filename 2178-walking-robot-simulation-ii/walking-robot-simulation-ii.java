d279830fc5340d515f084c9dd039201cc28b3a52
dharshika-07 <dharshikag2006@gmail.com>
2026-04-07 22:46:41 +0530

    Time: 65 ms (66.43%) | Memory: 57.9 MB (69.93%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -1,48 +1,49 @@
+// Added using AI
 class Robot {
-    private int width, height, perimeter;
-    private int x, y, dir;
-
-    private int[] dx = {1, 0, -1, 0};
-    private int[] dy = {0, 1, 0, -1};
-    private String[] dirs = {"East", "North", "West", "South"};
+    int x, y, width, height;
+    String dir;
 
     public Robot(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.perimeter = 2 * (width + height) - 4;
-
-        this.x = 0;
-        this.y = 0;
-        this.dir = 0;
+        this.x = 0; this.y = 0;
+        this.dir = "East" ;
+        this.width = width; this.height = height;
     }
 
     public void step(int num) {
-        num %= perimeter;
-
-        if (num == 0) {
-            num = perimeter;
-        }
+        int perim = 2 * (width - 1) + 2 * (height - 1);
+        num %= perim;
+        if (num == 0) num = perim;
 
         while (num > 0) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
-                dir = (dir + 1) % 4;
-                continue;
+            int nx = x, ny = y;
+            if (dir.equals("East")) {
+                int maxX = Math.min(x + num, width - 1);
+                int rem  = num - (maxX - x) ;
+                num = rem;
+                if (rem == 0) x = maxX;
+                else          { x = maxX; dir = "North"; }
+            } else if (dir.equals("West")) {
+                int minX = Math.max(x - num, 0);
+                int rem  = num - (x - minX) ;
+                num = rem;
+                if (rem == 0) x = minX;
+                else          { x = minX; dir = "South"; }
+            } else if (dir.equals("North")) {
+                int maxY = Math.min(y + num, height - 1);
+                int rem  = num - (maxY - y) ;
+                num = rem;
+                if (rem == 0) y = maxY;
+                else          { y = maxY; dir = "West"; }
+            } else if (dir.equals("South")) {
+                int minY = Math.max(y - num, 0);
+                int rem  = num - (y - minY) ;
+                num = rem;
+                if (rem == 0) y = minY;
+                else          { y = minY; dir = "East"; }
             }
-
-            x = nx;
-            y = ny;
-            num--;
         }
     }
 
-    public int[] getPos() {
-        return new int[]{x, y};
-    }
-
-    public String getDir() {
-        return dirs[dir];
-    }
+    public int[] getPos() { return new int[]{x, y}; }
+    public String getDir() { return dir; }
 }
